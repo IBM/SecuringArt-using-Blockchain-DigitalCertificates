aae1c340a92201fe526ad67c479a8b7a21d61c55
LauraBennett <lbenn@us.ibm.com>
2018-08-15 12:10:01 -0700

    initial commit

---
+++ quicktest.sh
@@ -0,0 +1,165 @@
+#!/bin/bash
+#
+# Copyright IBM Corp. All Rights Reserved.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+jq --version > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+	echo "Please Install 'jq' https://stedolan.github.io/jq/ to execute this script"
+	echo
+	exit 1
+fi
+
+starttime=$(date +%s)
+
+# Language defaults to "golang"
+LANGUAGE="golang"
+CC_SRC_PATH="github.com/auction"
+
+echo "POST request Enroll on Org1  ..."
+echo
+ORG1_TOKEN=$(curl -s -X POST \
+  http://localhost:4000/users \
+  -H "content-type: application/x-www-form-urlencoded" \
+  -d 'username=Jim&orgName=Org1')
+echo $ORG1_TOKEN
+ORG1_TOKEN=$(echo $ORG1_TOKEN | jq ".token" | sed "s/\"//g")
+echo
+echo "ORG1 token is $ORG1_TOKEN"
+echo
+echo "POST request Enroll on Org2 ..."
+echo
+ORG2_TOKEN=$(curl -s -X POST \
+  http://localhost:4000/users \
+  -H "content-type: application/x-www-form-urlencoded" \
+  -d 'username=Barry&orgName=Org2')
+echo $ORG2_TOKEN
+ORG2_TOKEN=$(echo $ORG2_TOKEN | jq ".token" | sed "s/\"//g")
+echo
+echo "ORG2 token is $ORG2_TOKEN"
+echo
+echo
+echo "POST request Create channel  ..."
+echo
+curl -s -X POST \
+  http://localhost:4000/channels \
+  -H "authorization: Bearer $ORG1_TOKEN" \
+  -H "content-type: application/json" \
+  -d '{
+	"channelName":"mychannel",
+	"channelConfigPath":"../artifacts/channel/mychannel.tx"
+}'
+echo
+echo
+sleep 5
+echo "POST request Join channel on Org1"
+echo
+curl -s -X POST \
+  http://localhost:4000/channels/mychannel/peers \
+  -H "authorization: Bearer $ORG1_TOKEN" \
+  -H "content-type: application/json" \
+  -d '{
+	"peers": ["peer0.org1.example.com","peer1.org1.example.com"]
+}'
+echo
+echo
+
+echo "POST request Join channel on Org2"
+echo
+curl -s -X POST \
+  http://localhost:4000/channels/mychannel/peers \
+  -H "authorization: Bearer $ORG2_TOKEN" \
+  -H "content-type: application/json" \
+  -d '{
+	"peers": ["peer0.org2.example.com","peer1.org2.example.com"]
+}'
+echo
+echo
+
+echo "POST Install chaincode on Org1"
+echo
+curl -s -X POST \
+  http://localhost:4000/chaincodes \
+  -H "authorization: Bearer $ORG1_TOKEN" \
+  -H "content-type: application/json" \
+  -d "{
+	\"peers\": [\"peer0.org1.example.com\",\"peer1.org1.example.com\"],
+	\"chaincodeName\":\"mycc\",
+	\"chaincodePath\":\"$CC_SRC_PATH\",
+	\"chaincodeType\": \"$LANGUAGE\",
+	\"chaincodeVersion\":\"v0\"
+}"
+echo
+echo
+
+echo "POST Install chaincode on Org2"
+echo
+curl -s -X POST \
+  http://localhost:4000/chaincodes \
+  -H "authorization: Bearer $ORG2_TOKEN" \
+  -H "content-type: application/json" \
+  -d "{
+	\"peers\": [\"peer0.org2.example.com\",\"peer1.org2.example.com\"],
+	\"chaincodeName\":\"mycc\",
+	\"chaincodePath\":\"$CC_SRC_PATH\",
+	\"chaincodeType\": \"$LANGUAGE\",
+	\"chaincodeVersion\":\"v0\"
+}"
+echo
+echo
+
+echo "POST instantiate chaincode on peer1 of Org1"
+echo
+curl -s -X POST \
+  http://localhost:4000/channels/mychannel/chaincodes \
+  -H "authorization: Bearer $ORG1_TOKEN" \
+  -H "content-type: application/json" \
+  -d "{
+	\"chaincodeName\":\"mycc\",
+	\"chaincodeVersion\":\"v0\",
+	\"chaincodeType\": \"$LANGUAGE\",
+	\"args\":[\"a\",\"100\",\"b\",\"200\"]
+}"
+echo
+echo
+
+saveImageOnBlockchain() {
+	cat <<EOF
+{
+    "itemDescription": "The Mona Lisa is a half-length portrait painting by the Italian Renaissance artist Leonardo da Vinci that has been described as the best known, the most visited, the most written about, the most sung about, the most parodied work of art in the world. The Mona Lisa is also one of the most valuable paintings in the world. It holds the Guinness World Record for the highest known insurance valuation in history at $100 million in 1962, which is worth nearly $800 million in 2017",
+    "itemDetail":"Monalisa",
+    "itemDate":"2018-04-23 12:23:13",
+    "itemType":"Original",
+    "itemSubject":"Portrait",
+    "itemMedia":"Canvas",
+    "itemSize":"15 x 15 in",
+    "itemImageType":"image/jpeg",
+    "itemBasePrice":"200000",
+    "itemImage":"data:image/jpeg;base64,$(base64 -w 0 images/Monalisa.jpg)",
+    "peers": "peer0.org1.example.com",
+    "fcn" : "saveImage"
+}
+EOF
+}
+
+echo "POST invoke chaincode on peers of Org1"
+echo
+curl -s -X POST \
+  http://localhost:4000/channels/mychannel/chaincodes/mycc \
+  -H "authorization: Bearer $ORG1_TOKEN" \
+  -H "content-type: application/json" \
+  -d "$(saveImageOnBlockchain)" | tee /tmp/imagedetails.json
+
+IMAGE_ID=$(jq -r .itemID /tmp/imagedetails.json)
+printf "\n\nImage ID : $IMAGE_ID\n\n"
+echo "GET query chaincode on peer1 of Org1"
+echo "image data is compressed and saved as thumbnail in public/images directory"
+echo
+curl -s -X GET \
+  "http://localhost:4000/channels/mychannel/chaincodes/mycc?peer=peer0.org1.example.com&fcn=getImageByID&args=${IMAGE_ID}" \
+  -H "authorization: Bearer $ORG1_TOKEN" \
+  -H "content-type: application/json" | jq 'del(.itemImage)' ### For the sake of simplicity deleting image data
+echo
+echo
